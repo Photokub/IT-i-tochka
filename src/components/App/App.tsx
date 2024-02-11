9a7d9c74a99b82a6a6cb2184386e0473d742e437
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-02-11 18:29:31 +0300

    Feat: верстать компонент Main

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from '../Main/Main';
+
+function App() {
+  return (
+    <div className="app">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MainImage from "../../images/pic_main.svg"
+import "./Main.css"
+
+interface MainPtopTypes { }
+
+const Main: React.FC<MainPtopTypes> = ({ }) => {
+    return (
+        <section className="main">
+            <div className="main__wrap">
+                <div className="main__container">
+                    <h1 className="main__title">Программируй правильно с <span className="main__title main__highlight">«IT и точка»</span></h1>
+                    <p className="main__paragraph" id="firstMainRow">Обучаем детей от 7 до 17 лет по методикам разработанными выпускниками и преподавателями из IT сферы.</p>
+                    <label htmlFor="" className="main__buttonSection">
+                        Запишись на бесплатное пробное занятие
+                        <button className="main__button">Записаться на пробный урок</button>
+                    </label>
+                    <p className="main__paragraph" id="thirdMainRow">Приведи друга и получи скидку на обучение!</p>
+                    <h3 className="main__subtitle">Каждую неделю <span className="main__highlight">бесплатное пробное занятие</span> для будущих программистов</h3>
+                    <img src={MainImage} alt="Иллюстрация" className="main__image" />
+                </div>
+            </div>
+            <div className="main__backgroundImage"></div>
+        </section>
+    )
+}
+
+export default Main;
